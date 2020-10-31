ba44ad6af5615142a5f1f46b626240bce3290876
Samyak Jain <samyak.jn11@gmail.com>
2020-10-31 20:00:00 +0530

    Adds function for allowed_file and allowed_file_filesize

--- app/views.py
+++ app/views.py
@@ -18,4 +18,22 @@ app.config["ALLOWED_FILE_EXTENSIONS"] = {"XML", "CSV", "XLSX", "xlsx"}
 app.config['MAX_CONTENT_LENGTH'] = 700 * 1024 * 1024
 xmlDocument = r'instance/uploads/'
 
+def get_namespace(element):
+    m = re.match('\{.*\}', element.tag)
+    return m.group(0) if m else ''
 
+def allowed_file(filename):
+    if not "." in filename:
+        return False
+    # for the extension
+    ext = filename.rsplit(".", 1)[1]
+    if ext.upper() in app.config["ALLOWED_FILE_EXTENSIONS"]:
+        return True
+    else:
+        return False
+
+def allowed_file_filesize(filesize):
+    if int(filesize) <= app.config["MAX_FILE_FILESIZE"]:
+        return True
+    else:
+        return False
